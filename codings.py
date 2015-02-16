258989ae596a896b3afabb9ebc4462fbec5bb9c6
EvanOman <evan058@gmail.com>
2015-02-16 14:06:45 -0600

    Adding coding file

---
+++ codings.py
@@ -0,0 +1,33 @@
+import sys
+
+def singpert(x, n, d, c, beta):
+	return x**n + c + beta/(x**d)
+
+def makeEncodings(n ,d, beta, minX, maxX, iterNum):
+	critR = beta**(1/(n + d))
+	scaleIter = (float(maxX) - float(minX))/iterNum
+
+	for i in xrange(iterNum):
+		c = maxX - scaleIter * i
+		codingStr = ""
+		myMap = lambda x: singpert(x, n, d, c, beta)
+		xVal = critR
+		for i in xrange(10):
+			if xVal > critR:
+				codingStr += " R "
+				continue
+			elif xVal == critR:
+				codingStr += " C "
+			elif xVal > 0 and xVal < critR:
+				codingStr += " L "
+			elif xVal < 0 and xVal > -critR:
+				codingStr += " r "
+			elif xVal < 0 and xVal < -critR:
+				codingStr += " l "
+			xVal = myMap(xVal)
+		print(codingStr + ":\t\t" + str(c))
+
+
+
+if __name__ == "__main__":
+	makeEncodings(2, 2, .001, float(sys.argv[1]), float(sys.argv[2]), int(sys.argv[3]))
